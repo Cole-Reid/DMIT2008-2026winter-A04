05c6c5d888beea5f8235532f018dd9cdff9e03a4
Cole Reid <creid59@nait.ca>
2026-02-11 11:52:03 -0700

    Modified page and component content

---
+++ 04 - react intro/examples/02 - react + nextjs components/student/components-example/app/components/Hello.js
@@ -0,0 +1,6 @@
+export default function Hello() {
+    // This is a static component meaning it returns the same thing every time
+    return (
+        <h3>Hey, here's our first component.</h3>
+    )
+}
---
+++ 04 - react intro/examples/02 - react + nextjs components/student/components-example/app/components/NewConcept.js
@@ -0,0 +1,6 @@
+export default function NewConcept(props) {
+
+    return (
+        <p>In this class, we'll learn {props.concept}</p>
+    )
+}
