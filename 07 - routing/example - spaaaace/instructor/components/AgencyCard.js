11ae0a128321ca80414ef7c7dd1d6f7f09127bd3
oaao <oliver+github@abolish.golf>
2026-03-31 09:45:27 -0600

    rev [07 - routing]: ex - set up click navigation to agency details page

--- 07 - routing/example - spaaaace/instructor/components/AgencyCard.js
+++ 07 - routing/example - spaaaace/instructor/components/AgencyCard.js
@@ -1,8 +1,8 @@
-/* reference for material ui components used
-https://mui.com/material-ui/react-card/#media
+// nextjs routing
+import { useRouter } from 'next/router';
 
-*/
-
+// mui components
+// reference for Card stuff: https://mui.com/material-ui/react-card/#media
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -11,6 +11,15 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 export default function AgencyCard({ id, imageUrl, name, abbreviation, description }) {
+
+    const router = useRouter()
+
+    const navigateToAgencyPage = () => {
+      // router.push: programmatically navigate to new URL path
+      //              I *could* wrap in a Link here, but want to demo this.
+      router.push(`/agency/${id}`)
+    }
+
     return <Card sx={{ marginTop: "8px", maxWidth: 345 }}>
     {imageUrl && <CardMedia
       component="img"
@@ -30,7 +39,12 @@ export default function AgencyCard({ id, imageUrl, name, abbreviation, descripti
       </Typography>
     </CardContent>
     <CardActions>
-      <Button size="small">Go to Agency</Button>
+      <Button
+        size="small"
+        onClick={navigateToAgencyPage}
+      >
+        Go to Agency
+      </Button>
     </CardActions>
   </Card>
 }
